3be269ba4b445c6a1930cdeb61308e9d4d1524b3
Ajini Perera <sahejanaperera@students.nsbm.lk>
2020-10-08 00:23:27 +0530

    Java Inheritance (OOP)

---
+++ Bounceable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lec06e01;
+
+/**
+ *
+ * @author Ajini Sahejana
+ */
+public interface Bounceable{   //public interface Bounceable extends Speaker{} is also valid
+    
+    public void bounce();
+}
